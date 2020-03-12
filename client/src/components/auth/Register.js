e386c90c5a089f582a185edcf9415d75f93e27df
Evan McPheron <evan.mcpheron@icloud.com>
2020-03-12 08:10:59 -0700

    Routes and first api call set

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,110 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = event =>
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+
+    const onSubmit = async event => {
+        event.preventDefault();
+        if (password !== password2) {
+            console.log('Passwords do not match');
+        } else {
+            const newUser = {
+                name,
+                email,
+                password
+            };
+
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+
+                const body = JSON.stringify(newUser);
+
+                const res = await axios.post('/api/users', body, config);
+                console.log(res.data);
+            } catch (error) {
+                console.error(error.response.data);
+            }
+        }
+    };
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Create Your Account
+            </p>
+            <form className="form" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={name}
+                        onChange={event => onChange(event)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={event => onChange(event)}
+                        name="email"
+                        required
+                    />
+                    <small className="form-text">
+                        This site uses Gravatar so if you want a profile image,
+                        use a Gravatar email
+                    </small>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={event => onChange(event)}
+                        minLength="6"
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        value={password2}
+                        onChange={event => onChange(event)}
+                        minLength="6"
+                    />
+                </div>
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Register"
+                />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="login.html">Sign In</Link>
+            </p>
+        </Fragment>
+    );
+};
+
+export default Register;
