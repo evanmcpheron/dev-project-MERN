95dbcf131746c58477f2639ef97e1f6495410a29
Evan McPheron <evan.mcpheron@icloud.com>
2020-03-24 09:43:50 -0700

    fixed comment security bug

--- client/src/components/tutorial/CommentForm.js
+++ client/src/components/tutorial/CommentForm.js
@@ -3,37 +3,49 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addComment } from '../../actions/tutorial';
 
-const CommentForm = ({ tutorialId, videoId, addComment }) => {
+const CommentForm = ({
+    tutorialId,
+    videoId,
+    addComment,
+    auth: { isAuthenticated }
+}) => {
     const [text, setText] = useState('');
 
     return (
         <div className="post-form">
-            <div className="bg-primary p">
-                <h3>Leave a Comment</h3>
-            </div>
-            <form
-                className="form my-1"
-                onSubmit={e => {
-                    e.preventDefault();
-                    addComment(videoId, tutorialId, { text });
-                    setText('');
-                }}
-            >
-                <textarea
-                    name="text"
-                    cols="30"
-                    rows="5"
-                    placeholder="Create a post"
-                    value={text}
-                    onChange={e => setText(e.target.value)}
-                    required
-                />
-                <input
-                    type="submit"
-                    className="btn btn-dark my-1"
-                    value="Submit"
-                />
-            </form>
+            {isAuthenticated ? (
+                <div>
+                    <div className="bg-primary p">
+                        <h3>Leave a Comment</h3>
+                    </div>
+                    <form
+                        className="form my-1"
+                        onSubmit={e => {
+                            e.preventDefault();
+                            addComment(videoId, tutorialId, { text });
+                            setText('');
+                        }}
+                    >
+                        <textarea
+                            name="text"
+                            cols="30"
+                            rows="5"
+                            placeholder="Create a post"
+                            value={text}
+                            onChange={e => setText(e.target.value)}
+                            required
+                        />
+
+                        <input
+                            type="submit"
+                            className="btn btn-dark my-1"
+                            value="Submit"
+                        />
+                    </form>
+                </div>
+            ) : (
+                <h2>You must be logged in to post comments</h2>
+            )}
         </div>
     );
 };
@@ -42,4 +54,8 @@ CommentForm.propTypes = {
     addComment: PropTypes.func.isRequired
 };
 
-export default connect(null, { addComment })(CommentForm);
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { addComment })(CommentForm);
