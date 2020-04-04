f9440e5ac3eba4cf8a7e3d70eb60d156ce6a41c3
Evan McPheron <evan.mcpheron@icloud.com>
2020-04-04 06:47:09 -0700

    added google analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
 // Components
 import Navbar from './components/layout/Navbar';
@@ -29,98 +31,84 @@ import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
+ReactGA.initialize('UA-80246531-5', {
+   debug: false,
+   titleCase: false,
+   gaOptions: {
+      userId: 123,
+   },
+});
+
+const history = createBrowserHistory();
+history.listen((location) => {
+   ReactGA.set({ page: location.pathname });
+   ReactGA.pageview(location.pathname);
+});
+
 if (localStorage.token) {
-    setAuthToken(localStorage.token);
+   setAuthToken(localStorage.token);
 }
 
 const App = () => {
-    useEffect(() => {
-        store.dispatch(loadUser());
-    }, []);
+   useEffect(() => {
+      store.dispatch(loadUser());
+   }, []);
 
-    return (
-        <Provider store={store}>
-            <Router>
-                <Fragment>
-                    <Navbar />
-                    <Route exact path="/" component={Landing} />
-                    <section>
-                        <Alert />
-                        <Switch>
-                            <Route exact path="/login" component={Login} />
-                            <Route
-                                exact
-                                path="/register"
-                                component={Register}
-                            />
-                            <Route
-                                exact
-                                path="/profiles"
-                                component={Profiles}
-                            />
-                            <Route
-                                exact
-                                path="/profile/:id"
-                                component={Profile}
-                            />
-                            <Route
-                                exact
-                                path="/tutorials"
-                                component={Tutorials}
-                            />
-                            <Route
-                                exact
-                                path="/tutorial/:id"
-                                component={Tutorial}
-                            />
-                            <PrivateRoute
-                                exact
-                                path="/dashboard"
-                                component={Dashboard}
-                            />
-                            <PrivateRoute
-                                exact
-                                path="/post/:id"
-                                component={Post}
-                            />
-                            <PrivateRoute
-                                exact
-                                path="/posts"
-                                component={Posts}
-                            />
-                            <PrivateRoute
-                                exact
-                                path="/create-profile"
-                                component={CreateProfile}
-                            />
-                            <PrivateRoute
-                                exact
-                                path="/add-experience"
-                                component={AddExperience}
-                            />
-                            <PrivateRoute
-                                exact
-                                path="/add-education"
-                                component={AddEducation}
-                            />
-                            <PrivateRoute
-                                exact
-                                path="/edit-profile"
-                                component={EditProfile}
-                            />
+   return (
+      <Provider store={store}>
+         <Router history={history}>
+            <Fragment>
+               <Navbar />
+               <Route exact path="/" component={Landing} />
+               <section>
+                  <Alert />
+                  <Switch>
+                     <Route exact path="/login" component={Login} />
+                     <Route exact path="/register" component={Register} />
+                     <Route exact path="/profiles" component={Profiles} />
+                     <Route exact path="/profile/:id" component={Profile} />
+                     <Route exact path="/tutorials" component={Tutorials} />
+                     <Route exact path="/tutorial/:id" component={Tutorial} />
+                     <PrivateRoute
+                        exact
+                        path="/dashboard"
+                        component={Dashboard}
+                     />
+                     <PrivateRoute exact path="/post/:id" component={Post} />
+                     <PrivateRoute exact path="/posts" component={Posts} />
+                     <PrivateRoute
+                        exact
+                        path="/create-profile"
+                        component={CreateProfile}
+                     />
+                     <PrivateRoute
+                        exact
+                        path="/add-experience"
+                        component={AddExperience}
+                     />
+                     <PrivateRoute
+                        exact
+                        path="/add-education"
+                        component={AddEducation}
+                     />
+                     <PrivateRoute
+                        exact
+                        path="/edit-profile"
+                        component={EditProfile}
+                     />
 
-                            <AdminRoute exact path="/admin" component={Admin} />
-                            <AdminRoute
-                                exact
-                                path="/admin/tutorial/:id"
-                                component={Videos}
-                            />
-                        </Switch>
-                    </section>
-                </Fragment>
-            </Router>
-        </Provider>
-    );
+                     <AdminRoute exact path="/admin" component={Admin} />
+                     <AdminRoute
+                        exact
+                        path="/admin/tutorial/:id"
+                        component={Videos}
+                     />
+                  </Switch>
+               </section>
+            </Fragment>
+         </Router>
+      </Provider>
+   );
 };
 
 export default App;
