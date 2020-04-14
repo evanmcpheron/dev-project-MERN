7a3f5339f2b8b02d12bf77dc8190cdcc278386a0
Evan McPheron <evan.mcpheron@icloud.com>
2020-04-14 11:10:20 -0700

    fixing navbar bug

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -3,6 +3,16 @@ import spinner from '../../img/spinner.gif';
 
 export default () => (
   <Fragment>
-    <i className="fas fa-spinner"></i>
+    <div className="container-spinner">
+      <div className="loader">
+        <div className="loader--dot"></div>
+        <div className="loader--dot"></div>
+        <div className="loader--dot"></div>
+        <div className="loader--dot"></div>
+        <div className="loader--dot"></div>
+        <div className="loader--dot"></div>
+        <div className="loader--dot"></div>
+      </div>
+    </div>
   </Fragment>
 );
