6003704d08236c088c25a08b3481907709c148f6
Evan McPheron <evan.mcpheron@icloud.com>
2020-04-23 09:29:27 -0700

    working on responsive profile

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -4,41 +4,39 @@ import { connect } from 'react-redux';
 import { addComment } from '../../actions/post';
 
 const CommentForm = ({ postId, addComment }) => {
-    const [text, setText] = useState('');
+  const [text, setText] = useState('');
 
-    return (
-        <div>
-            <div>
-                <h5>Leave a Comment</h5>
-            </div>
-            <form
-                onSubmit={e => {
-                    e.preventDefault();
-                    addComment(postId, { text });
-                    setText('');
-                }}
-            >
-                <textarea
-                    name="text"
-                    cols="30"
-                    rows="5"
-                    placeholder="Create a post"
-                    value={text}
-                    onChange={e => setText(e.target.value)}
-                    required
-                />
-                <input
-                    className="btn btn-success"
-                    type="submit"
-                    value="Submit"
-                />
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <h5>Leave a Comment</h5>
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addComment(postId, { text });
+          setText('');
+        }}
+      >
+        <textarea
+          name="text"
+          cols="30"
+          rows="5"
+          placeholder="Create a post"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          required
+        />
+        <button className="btn btn-success" type="submit" value="Submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 CommentForm.propTypes = {
-    addComment: PropTypes.func.isRequired
+  addComment: PropTypes.func.isRequired,
 };
 
 export default connect(null, { addComment })(CommentForm);
